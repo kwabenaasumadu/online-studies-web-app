6107dc005e68a2abc1f62f27a011ab3e009ebbf4
Kwabena <kasumadu@tapnhop.com>
2024-04-25 02:17:20 -0700

    first commit

---
+++ src/pages/comps/first-Header/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "../../../styles/first-Header.module.css";
+
+function Index() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.container_items}>
+          <div className={styles.container_item_1}>
+            <h1>Cod E-learning</h1>
+            <span>Online education & learning</span>
+          </div>
+
+          <div className={styles.container_item_2}>
+            <h1>Cod E-learning</h1>
+            <span>Online education & learning</span>
+          </div>
+
+          <div className={styles.container_item_3}>
+            <h1>Cod E-learning</h1>
+            <span>Online education & learning</span>
+          </div>
+
+          <div className={styles.container_item_4}>
+            <h1>Cod E-learning</h1>
+            <span>Online education & learning</span>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Index;
