d989d71cdccd2efb68cb61c7ad0e1561a146e8a5
Kwabena <kasumadu@tapnhop.com>
2024-04-29 10:28:24 -0700

    modifying the env

--- firebase.config.js
+++ firebase.config.js
@@ -3,19 +3,18 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env?.REACT_FIREBASE_API_KEY,
-  authDomain: process.env?.REACT_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env?.REACT_FIREBASE_DATABASE_URL,
-  projectId: process.env?.REACT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env?.REACT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env?.REACT_FIREBASE_MESSAGING_SENDING_ID,
-  appId: process.env?.REACT_FIREBASE_APP_ID,
-  measurementId: process.env?.REACT_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDING_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
-
 
 export { db, app, auth };
