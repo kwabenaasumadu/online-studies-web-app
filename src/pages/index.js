ae8b506cb2d05b93b76d106297bed0081d064d18
Kwabena <kasumadu@tapnhop.com>
2024-04-29 03:12:37 -0700

    added support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Comps from "../pages/comps";
+import SupportIcon from "@mui/icons-material/SupportAgent";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Home() {
+  const [isSupportOpen, setIsSupportOpen] = useState(false);
   return (
     <>
       <Head>
@@ -14,7 +18,31 @@ export default function Home() {
       </Head>
       <main>
         <Comps />
+
+        <div
+          className={styles.support_container}
+          onClick={() => setIsSupportOpen(true)}
+        >
+          <SupportIcon className={styles.icon} />
+        </div>
       </main>
+
+      {isSupportOpen && (
+        <>
+          <div className={styles.container}>
+            <div
+              className={styles.container_close}
+              onClick={() => setIsSupportOpen(false)}
+            >
+              <CloseIcon className={styles.icon} />
+            </div>
+            <div className={styles.container_header}>
+              <h1>Have a Question?</h1>
+              <button>Chat with us</button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
