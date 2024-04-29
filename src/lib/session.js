01431368c4f5761738b428aa449dd16dbf908ffc
Kwabena <kasumadu@tapnhop.com>
2024-04-29 07:55:57 -0700

    solved login

--- src/lib/session.js
+++ src/lib/session.js
@@ -1,9 +1,14 @@
-import { withIronSession } from 'next-iron-session';
+import { withIronSession } from "next-iron-session";
 
 export default function withSession(handler) {
   return withIronSession(handler, {
     password: process.env.SECRET_COOKIE,
-    cookieName: 'cod_e_learning',
- 
+    cookieName: "cod_e_learning",
+
+    cookieOptions: {
+      maxAge: 1200,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      httpOnly: true,
+    },
   });
 }
