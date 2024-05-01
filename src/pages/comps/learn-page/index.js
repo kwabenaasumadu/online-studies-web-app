aecb30d021c86e9b38d224b0d14232eca9893ac6
Kwabena <kasumadu@tapnhop.com>
2024-05-01 08:16:59 -0700

    added learn page

--- src/pages/comps/learn-page/index.js
+++ src/pages/comps/learn-page/index.js
@@ -4,7 +4,192 @@ import styles from "./../../../styles/learn-page.module.css";
 function Index() {
   return (
     <>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.container_header}>
+          <h1>Welcome To introduction To Computer</h1>
+        </div>
+
+        <div className={styles.container_nav}>
+          <h1>Course 1</h1>
+          <h1>Course 2</h1>
+          <h1>Course 3</h1>
+          <h1>Course 4</h1>
+          <h1>Course 5</h1>
+          <h1>Course 6</h1>
+          <h1>Course 7</h1>
+          <h1>Course 8</h1>
+          <h1>Course 9</h1>
+          <h1>Course 10</h1>
+          <h1>Course 11</h1>
+          <h1>Course 12</h1>
+          <h1>Course 13</h1>
+        </div>
+
+        <div className={styles.container_content_container}>
+          <div className={styles.container_content_title}>
+            <h1>What is computer science</h1>
+          </div>
+
+          <div className={styles.container_content_body_video}>
+            <video controls></video>
+          </div>
+
+          <div className={styles.container_content_body}>
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you to create your first web page without spending the
+              entire tutorial focusing too much on the “why.” By the end of this
+              tutorial, you will have the know-how to create a basic website and
+              we hope that this will inspire you to delve further into the world
+              of HTML using our follow-on guides. Read more
+            </p>
+            <br />
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you to create your first web page without spending the
+              entire tutorial focusing too much on the “why.” By the end of this
+              tutorial, you will have the know-how to create a basic website and
+              we hope that this will inspire you to delve further into the world
+              of HTML using our follow-on guides. Read more While many guides on
+              the internet attempt to teach HTML using a lot of mind-boggling
+              theory, this tutorial will instead focus on giving you the
+              practical skills to build your first site. The aim is to show you
+              to create your first web page without spending the entire tutorial
+              focusing too much on the “why.” By the end of this tutorial, you
+              will have the know-how to create a basic website and we hope that
+              this will inspire you to delve further into the world of HTML
+              using our follow-on guides. Read more While many guides on the
+              internet attempt to teach HTML using a lot of mind-boggling
+              theory, this tutorial will instead focus on giving you the
+              practical skills to build your first site. The aim is to show you
+              to create your first web page without spending the entire tutorial
+              focusing too much on the “why.” By the end of this tutorial, you
+              will have the know-how to create a basic website and we hope that
+              this will inspire you to delve further into the world of HTML
+              using our follow-on guides. Read more
+            </p>
+
+            <br />
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you to create your first web page without spending the
+              entire tutorial focusing too much on the “why.” By the end of this
+              tutorial, you will have the know-how to create a basic website and
+              we hope that this will inspire you to delve further into the world
+              of HTML using our follow-on guides. Read more While many guides on
+              the internet attempt to teach HTML using a lot of mind-boggling
+              theory, this tutorial will instead focus on giving you the
+              practical skills to build your first site. The aim is to show you
+              to create your first web page without spending the entire tutorial
+              focusing too much on the “why.” By the end of this tutorial, you
+              will have the know-how to create a basic website and we hope that
+              this will inspire you to delve further into the world of HTML
+              using our follow-on guides. Read more While many guides on the
+              internet attempt to teach HTML using a lot of mind-boggling
+              theory, this tutorial will instead focus on giving you the
+              practical skills to build your first site. The aim is to show you
+              to create your first web page without spending the entire tutorial
+              focusing too much on the “why.” By the end of this tutorial, you
+              will have the know-how to create a basic website and we hope that
+              this will inspire you to delve further into the world of HTML
+              using our follow-on guides. Read more
+            </p>
+
+            <br/>
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more
+            </p>
+
+            <br/>
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more
+            </p>
+
+            <br/>
+            <p>
+              While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more   While many guides on the internet attempt to teach HTML using a
+              lot of mind-boggling theory, this tutorial will instead focus on
+              giving you the practical skills to build your first site. The aim
+              is to show you  to create your first web page without
+              spending the entire tutorial focusing too much on the “why.” By
+              the end of this tutorial, you will have the know-how to create a
+              basic website and we hope that this will inspire you to delve
+              further into the world of HTML using our follow-on guides. Read
+              more
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
